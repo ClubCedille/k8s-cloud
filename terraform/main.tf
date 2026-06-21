e8ba61331e22335d2f2e1913779083c42d159e47
Emilien Busson <emilienbusson@MacBook-Pro-de-Emilien.local>
2026-06-20 22:53:33 -0400

    add terraform and new workflow

---
+++ terraform/main.tf
@@ -0,0 +1,65 @@
+terraform{
+  required_providers {
+    google = {
+      source="hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+}
+
+resource "google_compute_instance" "gatus-vm" {
+  name = "ced-gce-gatus-prd-na1-01"
+  machine_type = "e2-micro"
+  zone = "northamerica-norhteast1-a"
+  labels = {
+    managed-by = "terraform"
+    env = "prd"
+    app = "gatus"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-13"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+
+  scheduling {
+    automatic_restart = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  service_account {
+    email = var.email_sa
+    scopes = ["cloud-plateform"]
+  }
+  metadata_startup_script = templatefile(abspath("${path.module}/../scripts/startup.sh.tftpl"), {
+    ssh_private_key = var.ssh_private_key
+  })
+  
+}
+
+resource "google_compute_firewall" "allow-httptrafic"{
+  name = "allow-httpgatus"
+  network = "default"
+
+  allow {
+    protocol = "tcp" 
+      ports = ["80", "443"]
+  }
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["gatus"]
+}
+
+output "gatus_ip" {
+    value = google_compute_instance.gatus-vm.network_interface.0.access_config.0.nat_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project"{
+  type = string
+  description = "project id"
+}
+
+variable "region" {
+  type = string
+  description = "region for the project"
+}
+
+variable "email_sa" {
+  type = string
+  description = "email service account. It's a confidential data"
+}
+
+variable "ssh_private_key" {
+  type = string
+  description = "ssh private key. Confidential data, place data in a secret."
+}
